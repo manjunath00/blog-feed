8c601adaed4d92ffc048b1a3bad1c434e359f805
manjunath00 <kadrimanjunath56@gmail.com>
2020-05-08 23:37:00 +0530

    user model completed

---
+++ models/user.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v1: uuidv1 } = require("uuid");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      maxlength: 32,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      unique: true,
+    },
+    dateOfBirth: {
+      type: Date,
+      required: true,
+    },
+    encry_password: {
+      type: String,
+      required: true,
+    },
+    // this salt is generated for new user
+    salt: {
+      type: String,
+    },
+    role: {
+      type: Number,
+      default: 0,
+    },
+    categoryPreferences: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema
+  .virtual("password")
+  .set(function (password) {
+    this._password = password;
+    this.salt = uuidv1();
+    this.encry_password = this.securePassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function (plainPassword) {
+    return this.securePassword(plainPassword) === this.encry_password
+      ? true
+      : false;
+  },
+  securePassword: function (plainPassword) {
+    if (!plainPassword) return "";
+    try {
+      return crypto
+        .createHmac("sha256", this.salt)
+        .update(plainPassword)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+};
+
+module.exports = mongoose.model("User", userSchema);
