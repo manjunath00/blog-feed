06896e5e6e3de4ffb122396b74850b8a7677b429
manjunath00 <kadrimanjunath56@gmail.com>
2020-05-08 23:36:09 +0530

    initial setup completed

---
+++ index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+
+const authRoutes = require("./routes/auth");
+const categoryRoutes = require("./routes/category");
+const articleRoutes = require("./routes/article");
+
+// database connections
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DATABASE CONNECTED");
+  })
+  .catch((error) => {
+    console.log("DAtabase not connected", error);
+  });
+
+// middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//routes
+app.use("/api", authRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", articleRoutes);
+
+app.get("/", (req, res) => {
+  res.send("<h2>Home screen</h2>");
+});
+
+//port
+const PORT = process.env.PORT;
+
+//Starting the server
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
