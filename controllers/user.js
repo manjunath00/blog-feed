46278e6862bc26c32becd1929729e7ea3b5e574e
manjunath00 <kadrimanjunath56@gmail.com>
2020-05-15 23:23:41 +0530

    added populate

--- controllers/user.js
+++ controllers/user.js
@@ -4,21 +4,23 @@ const { validationResult } = require("express-validator");
 
 // middleware to populate req.profile
 exports.getUserById = (req, res, next, userId) => {
-  User.findById(userId).exec((err, user) => {
-    if (err || !user) {
-      console.log(err, user);
-      return res.status(400).json({
-        error: "No user found in db",
-      });
-    }
+  User.findById(userId)
+    .populate("categoryPreferences", "_id categoryName")
+    .exec((err, user) => {
+      if (err || !user) {
+        console.log(err, user);
+        return res.status(400).json({
+          error: "No user found in db",
+        });
+      }
 
-    req.profile = user;
-    req.profile.salt = undefined;
-    req.profile.encry_password = undefined;
-    req.profile.createdAt = undefined;
-    req.profile.updatedAt = undefined;
-    next();
-  });
+      req.profile = user;
+      req.profile.salt = undefined;
+      req.profile.encry_password = undefined;
+      req.profile.createdAt = undefined;
+      req.profile.updatedAt = undefined;
+      next();
+    });
 };
 
 // get user
