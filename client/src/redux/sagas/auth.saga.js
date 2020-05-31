bc6dde4e8f0867dd9dc575ef8824f635a17ee6ad
manjunath00 <kadrimanjunath56@gmail.com>
2020-05-31 14:11:18 +0530

    [fix] removed token from redux store

--- client/src/redux/sagas/auth.saga.js
+++ client/src/redux/sagas/auth.saga.js
@@ -1,27 +1,32 @@
 /* eslint-disable */
 import { takeEvery, call, all, put, takeLatest } from "redux-saga/effects";
+import Cookies from "js-cookie";
 
 import { loginSuccess, loginFailed } from "../actions/login";
-import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE } from "../actions/types";
-
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE } from "../actions/types"; 
 import { loginApi } from "../../api/apicalls";
 
 function* loginAsync(action) {
   try {
     const response = yield call(loginApi, action.payload);
-    yield put(loginSuccess(response.data));
+    const auth = {
+      _id: response.data.user._id,
+      email: response.data.user.email,
+    };
+   
+    yield put(loginSuccess(auth));
   } catch (error) {
     yield put(loginFailed(error.message));
   }
 }
 
-function* logoutAsync() {}
+// function* logoutAsync() {}
 
-function* logoutWatcher() {
-  yield takeEvery(LOGIN_START, loginAsync);
-}
+// function* logoutWatcher() {
+//   yield takeEvery(LOGIN_START, loginAsync);
+// }
 
-export function* loginWatcher() {
+function* loginWatcher() {
   yield takeEvery(LOGIN_START, loginAsync);
 }
 
