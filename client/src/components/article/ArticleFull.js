45d323502045f4d17c8089ef9e8f224f64098c09
manjunath00 <kadrimanjunath56@gmail.com>
2020-05-31 14:07:38 +0530

    [feat] completed full view of an article

---
+++ client/src/components/article/ArticleFull.js
@@ -0,0 +1,83 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { getAnArticle } from "../../redux/actions/article";
+
+function ArticleFull({ match, getAnArticle, article }) {
+  const articleId = match.params.articleId;
+  useEffect(() => {
+    getAnArticle(articleId);
+  }, [articleId]);
+
+  if (article) {
+    const {
+      _id,
+      articleName,
+      articleBody,
+      categoryId,
+      authorId,
+      totalLikes,
+      totalDislikes,
+      createdAt,
+    } = article;
+    return (
+      <div class='article-item'>
+        <div class='article-item-second-row'>
+          <span>
+            <Link
+              to={`/category/${categoryId._id}`}
+              class='article-item-category'>
+              c/{categoryId.categoryName}
+            </Link>
+          </span>
+          <span class='article-item-meta'>
+            <span>Posted by </span>
+            <span>
+              <Link to={`/author/${authorId._id}`} class='article-item-author'>
+                {authorId.firstName}
+              </Link>
+            </span>
+            <span> 2 hours ago</span>
+          </span>
+        </div>
+        <div class='article-item-first-row'>
+          <Link class='article-item-heading' to={`/article/view/${_id}`}>
+            {articleName}
+          </Link>
+        </div>
+        <div class='article-item__body'>{articleBody}</div>
+
+        <div class='article-third-row'>
+          <span class='article-item-cta'>
+            <button>
+              <i class='fas fa-thumbs-up like'></i>
+            </button>
+            <span>{totalLikes}</span>
+          </span>
+          <span class='article-item-cta'>
+            <button>
+              <i class='fas fa-thumbs-down dislike'></i>
+            </button>
+            <span>{totalDislikes}</span>
+          </span>
+          <span class='article-item-cta'>
+            <button>
+              <i class='fas fa-ban block'></i>
+            </button>
+          </span>
+        </div>
+      </div>
+    );
+  } else {
+    return <div className='lds-dual-ring'> </div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    article: state.articles.article,
+  };
+};
+
+export default connect(mapStateToProps, { getAnArticle })(ArticleFull);
