1aa420cac75b5167440737314a2da8b2106061cf
manjunath00 <kadrimanjunath56@gmail.com>
2020-06-01 07:54:15 +0530

    [fix] wrapped in a container

--- client/src/components/article/ArticleFull.js
+++ client/src/components/article/ArticleFull.js
@@ -23,55 +23,59 @@ function ArticleFull({ match, getAnArticle, article }) {
       createdAt,
     } = article;
     return (
-      <div class='article-item'>
-        <div class='article-item-second-row'>
-          <span>
-            <Link
-              to={`/category/${categoryId._id}`}
-              class='article-item-category'>
-              c/{categoryId.categoryName}
-            </Link>
-          </span>
-          <span class='article-item-meta'>
-            <span>Posted by </span>
+      <div class='main-bar'>
+        <div class='article-item'>
+          <div class='article-item-second-row'>
             <span>
-              <Link to={`/author/${authorId._id}`} class='article-item-author'>
-                {authorId.firstName}
+              <Link
+                to={`/category/${categoryId._id}`}
+                class='article-item-category'>
+                c/{categoryId.categoryName}
               </Link>
             </span>
-            <span> 2 hours ago</span>
-          </span>
-        </div>
-        <div class='article-item-first-row'>
-          <Link class='article-item-heading' to={`/article/view/${_id}`}>
-            {articleName}
-          </Link>
-        </div>
-        <div class='article-item__body'>{articleBody}</div>
+            <span class='article-item-meta'>
+              <span>Posted by </span>
+              <span>
+                <Link
+                  to={`/author/${authorId._id}`}
+                  class='article-item-author'>
+                  {authorId.firstName}
+                </Link>
+              </span>
+              <span> 2 hours ago</span>
+            </span>
+          </div>
+          <div class='article-item-first-row'>
+            <Link class='article-item-heading' to={`/article/view/${_id}`}>
+              {articleName}
+            </Link>
+          </div>
+          <div class='article-item__body'>{articleBody}</div>
 
-        <div class='article-third-row'>
-          <span class='article-item-cta'>
-            <button>
-              <i class='fas fa-thumbs-up like'></i>
-            </button>
-            <span>{totalLikes}</span>
-          </span>
-          <span class='article-item-cta'>
-            <button>
-              <i class='fas fa-thumbs-down dislike'></i>
-            </button>
-            <span>{totalDislikes}</span>
-          </span>
-          <span class='article-item-cta'>
-            <button>
-              <i class='fas fa-ban block'></i>
-            </button>
-          </span>
+          <div class='article-third-row'>
+            <span class='article-item-cta'>
+              <button>
+                <i class='fas fa-thumbs-up like'></i>
+              </button>
+              <span>{totalLikes}</span>
+            </span>
+            <span class='article-item-cta'>
+              <button>
+                <i class='fas fa-thumbs-down dislike'></i>
+              </button>
+              <span>{totalDislikes}</span>
+            </span>
+            <span class='article-item-cta'>
+              <button>
+                <i class='fas fa-ban block'></i>
+              </button>
+            </span>
+          </div>
         </div>
       </div>
     );
   } else {
-    return <LoadingSpinner />
+    return <LoadingSpinner />;
   }
 }
 
