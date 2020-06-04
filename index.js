df1c4534f294feddf06cf2c7ade0926f39416e72
manjunath00 <kadrimanjunath56@gmail.com>
2020-06-04 14:15:40 +0530

    [fix] added path module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
