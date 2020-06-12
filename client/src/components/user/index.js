0f124741cde2bbf8349ecd7db501328aa60067a7
manjunath00 <kadrimanjunath56@gmail.com>
2020-06-13 00:25:12 +0530

    [fix] renamed file

--- client/src/components/user/index.js
+++ client/src/components/user/index.js
@@ -1,6 +1,6 @@
 import { Login, Signup } from "./auth";
 
-import Settings from "./settings/Setting";
+import Settings from "./settings/Settings";
 import Dashboard from "./dashboard/Dashboard";
 import AuthorFeed from "./AuthorFeed";
 
